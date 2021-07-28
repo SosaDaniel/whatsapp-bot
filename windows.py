d82258fe0808180d04ce0691a52145577c4b55f2
DanielSosa12 <sosa.danielb@gmail.com>
2021-07-28 00:17:58 -0300

    Modificando las vistas

--- windows.py
+++ windows.py
@@ -1,22 +1,33 @@
 from spam import spam_bot
 from tkinter import *
+from PIL import Image, ImageTk
 
 ventana = Tk()
 ventana.geometry("900x700")
 ventana.title("WhatsApp Bot")
 ventana.resizable(0,0)
 ventana.config(
-    bg= "lightgray"
+    bg= "#44C694"
 )
+ventana.iconbitmap('./images/icon3.ico')
+image = Image.open('./images/spam_image.png').resize((300, 300))
+render = ImageTk.PhotoImage(image)
+label_image = Label(ventana, image= render)
+label_image.config(bg="#44C694")
+
 
 # Pantallas
 
-home_frame = Frame(ventana)
+
+
 spam_frame = Frame(ventana)
 info_frame = Frame(ventana)
 
 
+
+
 def home():
+    
     return None
 
 def spam_window():
@@ -24,43 +35,77 @@ def spam_window():
     contact = StringVar()
     message = StringVar()
     amount  = IntVar()
-    
-    label_contact = Label(ventana, text="Nombre del contacto")
+    amount.set("")
+
+    label_image.place(x=550, y=50)
+
+    home_frame = Frame(ventana, width=350, height=350)
+    home_frame.config(
+    bg="#44C694",
+    bd=3
+    )
+    home_frame.place(x=30, y=380)
+
+    #Entry y labels CONTACTO
+
+    label_contact = Label(home_frame, text="Nombre del contacto")
     label_contact.config(
-        bg="lightgray",
+        bg="#44C694",
         justify= "left",
         padx=5, 
-        pady=5
+        pady=5, 
+        font = ("Century Gothic", 12)
     )
     label_contact.grid(column=1, row=1, sticky=E)
-    entry_contact = Entry(ventana, textvariable=contact).grid(column=3, row=1,padx=5, pady=5, sticky=W)
+    entry_contact = Entry(home_frame, textvariable=contact)
+    entry_contact.config(
+        width=30,
+        font= ("Century Gothic", 12)
+    )
+    entry_contact.grid(column=3, row=1,padx=5, pady=5, sticky=W)
 
-    label_message = Label(ventana, text="Mensaje")
+    #Entry y labels MENSAJE
+
+    label_message = Label(home_frame, text="Mensaje")
     label_message.config(
-        bg="lightgray",
-        justify= "left",
+        bg="#44C694",
+        justify= "right",
         padx=5, 
-        pady=5
+        pady=5,
+        font = ("Century Gothic", 12)
     )
-    label_message.grid(column=1, row=3, sticky=E)
-    entry_message = Entry(ventana, textvariable=message).grid(column=3, row=3, padx=5, pady=5, sticky=W)
+    label_message.grid(column=1, row=5, sticky=E)
+    entry_message = Entry(home_frame, textvariable=message)
+    entry_message.config(
+        width=30,
+        font= ("Century Gothic", 12)
+    )
+    entry_message.grid(column=3, row=5, padx=5, pady=5, sticky=W)
 
-    label_amount = Label(ventana, text="Cantidad de mensajes")
+    #Entry y labels CANTIDAD
+
+    label_amount = Label(home_frame, text="Cantidad de mensajes")
     label_amount.config(
-        bg="lightgray",
+        bg="#44C694",
         justify= "left",
         padx=5, 
-        pady=5
+        pady=5,
+        font = ("Century Gothic", 12)
     )
-    label_amount.grid(column= 1, row=5, sticky=E)
-    entry_amount = Entry(ventana, textvariable=amount).grid(column=3, row=5, padx=5, pady=5, sticky=W)
+    label_amount.grid(column= 1, row=3, sticky=E)
+    entry_amount = Entry(home_frame, textvariable=amount)
+    entry_amount.config(
+        width=30,
+        font= ("Century Gothic", 12)
+    )
+    entry_amount.grid(column=3, row=3, padx=5, pady=5, sticky=W)
     
-    button_run = Button(ventana, text= "Enviar", command= lambda:spam_bot(contact.get(), message.get(), int(amount.get())))
+    button_run = Button(home_frame, text= "Enviar", command= lambda:spam_bot(contact.get(), message.get(), int(amount.get())))
     button_run.config(
-        width=25,
-        height=2
+        width=18,
+        height=1
     )
-    button_run.grid(column = 3, row= 6, sticky=E)
+    button_run.grid(column = 3, row= 7, sticky=E)
 
 
 
